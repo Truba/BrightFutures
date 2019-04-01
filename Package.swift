445bd690ffde7abbf3e7ff6e8e6b899f8485a9a4
Vlaho <vlaho.poluta@infinum.hr>
2019-04-01 08:28:31 +0200

    Update package manager

--- Package.swift
+++ Package.swift
@@ -1,8 +1,29 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "BrightFutures",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "BrightFutures",
+            targets: ["BrightFutures"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 4),
+        .package(url: "https://github.com/antitypical/Result.git", from: "4.0.0")
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "BrightFutures",
+            dependencies: ["Result"]),
+        .testTarget(
+            name: "BrightFuturesTests",
+            dependencies: ["BrightFutures"]),
     ]
 )
